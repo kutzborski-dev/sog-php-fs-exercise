0d7b498772e2d9f9ed89d96b6e8bc02d07d26525
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-08-03 14:48:58 +0100

    (#4) Finish database and querying logic

---
+++ src/core/Database.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Core;
+
+use PDOException;
+use PDO;
+
+class Database {
+    private static ?PDO $instance = null;
+    private static string $dbDir;
+
+    public static function init(string $db = 'db.sqlite') {
+        if(self::$instance === null) {
+            self::$dbDir = realpath(__DIR__ . '/../data') .'/';
+    
+            self::connect($db);
+        }
+    }
+
+    private static function connect(string $db) {
+        $dbPath = 'sqlite:'. self::$dbDir . $db;
+
+        try {
+            self::$instance = new PDO($dbPath);
+            // Set error mode to exceptions
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            throw "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public static function query(string $sql, array $params = [])
+    {
+        $stmt = self::$instance->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+
+    public static function execute(string $sql, array $params = [])
+    {
+        $stmt = self::$instance->prepare($sql);
+        return $stmt->execute($params);
+    }
+}
